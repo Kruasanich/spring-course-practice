3853800d78234a2d826f8b19f38e0500dd417b6b
k.yakimchuk <k.hormunkul@gmail.com>
2021-07-27 21:03:14 +0300

    Change logic of printing Spring Beans.

--- src/main/java/ru/raiffeisen/study/spring/course/core/Application.java
+++ src/main/java/ru/raiffeisen/study/spring/course/core/Application.java
@@ -20,12 +20,9 @@ public class Application {
 
             System.out.println("Let's inspect the beans provided by Spring Boot:");
 
-            String[] beanNames = ctx.getBeanDefinitionNames();
-            Arrays.sort(beanNames);
-            for (String beanName : beanNames) {
-                System.out.println(beanName);
-            }
-
+            Arrays.stream(ctx.getBeanDefinitionNames())
+                    .sorted()
+                    .forEach(System.out::println);
         };
     }
 }
